2f5eb55e32bf74c8c8f49fa52e7deca6d51b8724
Tim Ward <timothyjward@apache.org>
2019-08-14 13:44:18 +0100

    Make JavaCPP into an OSGi bundle
     * Annotations are used to export packages and define versions
     * User implementable interfaces are marked `@ConsumerType`
     * API baselining is enabled
    
    Signed-off-by: Tim Ward <timothyjward@apache.org>

--- javacpp/src/main/java/org/bytedeco/javacpp/annotation/package-info.java
+++ javacpp/src/main/java/org/bytedeco/javacpp/annotation/package-info.java
@@ -1,4 +1,6 @@
 /**
  * Contains all the annotation classes used by JavaCPP.
  */
+@org.osgi.annotation.bundle.Export
+@org.osgi.annotation.versioning.Version("1.5.0")
 package org.bytedeco.javacpp.annotation;
