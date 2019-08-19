37e15c7cfed7b0ddc2cf9ae6ed4b8a32c114af51
Tim Ward <timothyjward@apache.org>
2019-08-19 12:47:41 +0100

    Add an integration test running in OSGi
        This test currently passes because it does not use the JavaCPP Loader to load its native libraries. Ideally this would be fixed so that users don't need to care about where they are running
    
        Signed-off-by: Tim Ward <timothyjward@apache.org>

---
+++ javacpp-tests-osgi/src/main/java/org/bytedeco/javacpp/test/osgi/Calc.java
@@ -0,0 +1,33 @@
+package org.bytedeco.javacpp.test.osgi;
+
+import org.bytedeco.javacpp.Loader;
+import org.bytedeco.javacpp.annotation.Platform;
+
+@Platform(include = "calc.h")
+public class Calc {
+    
+    static {
+        
+        // This line should be sufficient, but it
+        // isn't because the library gets loaded by
+        // the wrong classloader
+        //
+        // Loader.load();
+        
+        // This is what we need to happen in the scope
+        // of the bundle containing the native code, 
+        // not the JavaCPP bundle. Note that the call
+        // to the Loader is just to force a wiring to
+        // the JavaCPP package needed by the JNI code
+        
+        Loader.getPlatform();
+        
+        System.loadLibrary("jniCalc");
+    }
+    
+    private Calc() { }
+    
+
+    public static native int add(int a, int b);
+
+}
