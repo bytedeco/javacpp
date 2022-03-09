fb3c4a33fd43b6c6e9bd03b1def46f74fcbed035
Daniel Avila <masterav10@gmail.com>
2022-03-09 00:05:30 -0500

    Formatting now matches rest of JavaCPP.

--- src/test/java/org/bytedeco/javacpp/DaemonTest.java
+++ src/test/java/org/bytedeco/javacpp/DaemonTest.java
@@ -27,55 +27,32 @@ import org.junit.Test;
  *
  */
 @Platform(compiler = "cpp11", define = "NO_JNI_DETACH_THREAD", include = "DaemonTest.h")
-public class DaemonTest
-{
-    public static class Callback extends Pointer
-    {
+public class DaemonTest {
+    public static class Callback extends Pointer {
         /** Default native constructor. */
-        public Callback()
-        {
-            super((Pointer) null);
-            allocate();
-        }
-
+        public Callback() { super((Pointer) null); allocate(); }
         /** Native array allocator. Access with {@link Pointer#position(long)}. */
-        public Callback(long size)
-        {
-            super((Pointer) null);
-            allocateArray(size);
-        }
-
+        public Callback(long size){ super((Pointer) null); allocateArray(size); }
         /** Pointer cast constructor. Invokes {@link Pointer#Pointer(Pointer)}. */
-        public Callback(Pointer p)
-        {
-            super(p);
-        }
+        public Callback(Pointer p) { super(p); }
 
         private native void allocate();
-
         private native void allocateArray(long size);
 
-        @Override
-        public Callback position(long position)
-        {
+        @Override public Callback position(long position) { 
             return (Callback) super.position(position);
         }
 
-        @Override
-        public Callback getPointer(long i)
-        {
-            return new Callback((Pointer) this).offsetAddress(i);
+        @Override public Callback getPointer(long i) { 
+            return new Callback((Pointer) this).offsetAddress(i); 
         }
 
-        @Virtual(true)
-        public native void callback(int value);
+        @Virtual(true) public native void callback(int value);
     }
 
     static native void run(Callback callback, int count);
 
-    @BeforeClass
-    public static void setUpClass() throws Exception
-    {
+    @BeforeClass public static void setUpClass() throws Exception {
         System.out.println("Builder");
         Class c = DaemonTest.class;
         Builder builder = new Builder().classesOrPackages(c.getName());
@@ -85,18 +62,13 @@ public class DaemonTest
         Loader.load(c);
     }
 
-    @Test
-    public void testJNIThreadAttachFromNativeCallbacks()
-    {
+    @Test public void testJNIThreadAttachFromNativeCallbacks() {
         final int count = 10;
         final List<Integer> callbackValueRefs = new ArrayList<>();
         final List<Thread> threadRefs = new ArrayList<>();
 
-        Callback callback = new Callback()
-        {
-            @Override
-            public void callback(int value)
-            {
+        Callback callback = new Callback() {
+            @Override public void callback(int value) {
                 callbackValueRefs.add(Integer.valueOf(value));
                 threadRefs.add(Thread.currentThread());
             }
@@ -107,21 +79,17 @@ public class DaemonTest
         assertTrue(callbackValueRefs.size() == count);
         assertTrue(threadRefs.size() == count);
 
-        for (int i = 0; i < count; i++)
-        {
+        for (int i = 0; i < count; i++) {
             int value = callbackValueRefs.get(i);
             Thread callbackThread = threadRefs.get(i);
             
-            assertEquals("Callback->callback( ... )", i + 1, value);
+            assertEquals("Callback was not called", i + 1, value);
 
-            assertNotNull("Expected callback to be called.", callbackThread);
-            assertNotEquals("Expected callback to be called on a background thread.", Thread.currentThread(),
-                    callbackThread);
-            assertTrue("Expected callback thread to be daemon.", callbackThread.isDaemon());
+            assertNotEquals("Callback thread is not main", Thread.currentThread(), callbackThread);
+            assertTrue("Callback thread is daemon", callbackThread.isDaemon());
         }
         
-        for(int i = 1; i < count; i++)
-        {
+        for(int i = 1; i < count; i++) {
             Thread cbThread1 = threadRefs.get(i - 1);
             Thread cbThread2 = threadRefs.get(i);
             
