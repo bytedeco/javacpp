8e22d43044d889a141fc98c9df3ac9a1a327dd56
Hervé Guillemet <hg@apteryx.fr>
2023-04-19 15:34:33 +0200

    Rename nativeModifiers to modifiers2

--- src/main/java/org/bytedeco/javacpp/tools/Parser.java
+++ src/main/java/org/bytedeco/javacpp/tools/Parser.java
@@ -2595,7 +2595,7 @@ public class Parser {
                               +  "_" + dcl.javaName + (dcl.parameters.names == null ? "()" : dcl.parameters.names) + "; }\n";
                     dcl.javaName = "_" + dcl.javaName;
                 }
-                decl.text += nativeModifiers + type.annotations + context.shorten(type.javaName) + " " + dcl.javaName + dcl.parameters.list + ";\n";
+                decl.text += modifiers2 + type.annotations + context.shorten(type.javaName) + " " + dcl.javaName + dcl.parameters.list + ";\n";
             }
             decl.signature = dcl.signature;
 
