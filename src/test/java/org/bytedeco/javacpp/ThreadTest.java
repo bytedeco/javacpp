e1a2b34dcfc8412c0c197eb6ee2a52df188a3dca
Samuel Audet <samuel.audet@gmail.com>
2026-02-24 12:14:58 +0900

    Update version in the `pom.xml` files to 1.5.14-SNAPSHOT

--- src/test/java/org/bytedeco/javacpp/ThreadTest.java
+++ src/test/java/org/bytedeco/javacpp/ThreadTest.java
@@ -102,7 +102,7 @@ public class ThreadTest {
 
         Thread.sleep(1000);
 
-        boolean buggyJDK = Loader.getPlatform().equals("macosx-x86_64");
+        boolean buggyJDK = Loader.getPlatform().startsWith("macosx");
 
         // thread should be automatically detached upon completion
         assertFalse(!buggyJDK && threadRefs.get(0).isAlive());
